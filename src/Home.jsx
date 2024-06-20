9345b63c91757aaedde37fba5839161805b9c5f9
Nikolas-Rodrigues <cruz.nikolasr@gmail.com>
2024-06-19 21:41:46 -0300

    A

--- src/Home.jsx
+++ src/Home.jsx
@@ -8,40 +8,8 @@ function Home() {
   const [nome, setNome] = useState('');
   const [link, setLink] = useState('');
   const [data, setData] = useState('');
-  const [dia, setDia] = useState('');
-  const [senha, setSenha] = useState('');
-  const [modalSit, setModalSit] = useState(false);
-  const segredo = "";
 
 
-
-
-
-  function excluirConf() {
-    const dados = { dia }
-    if (senha === segredo) {
-      axios.delete(`${host}/apagarRegistro`, dados)
-        .then(response => {
-          console.log('Conferência apagado com sucesso:', response.data);
-          alert("Conferência excluida com sucesso");
-        })
-        .catch(error => {
-          console.error('Erro ao apagar Conferência:', error);
-          alert("Erro ao apagar Conferência");
-
-        });
-    }
-    setModalSit(false);
-  }
-
-
-  function abrirModal() {
-    setModalSit(true);
-  }
-
-  function fecharModal() {
-    setModalSit(false);
-  }
 
   function addConferencia() {
     let dados = { nome, link, data };
@@ -93,42 +61,6 @@ function Home() {
         </div >
       </div>
 
-      <div className='contentApagar'>
-        <button className='apagar' onClick={abrirModal}>
-          <span>Remover</span>
-        </button>
-      </div>
-
-      {modalSit && (
-        <div className='modal'>
-          <div className='modalContent'>
-            <div>
-              <span>Senha:</span>
-            </div>
-            <p></p>
-            <div>
-              <input type='text' onChange={(evento) => setSenha(evento.target.value)} ></input>
-            </div>
-            <p></p>
-            <div>
-              <span>Data a ser removida a conferencia:</span>
-            </div>
-            <p></p>
-            <div>
-              <input type='date' id='dia' onChange={(evento) => setDia(evento.target.value)}></input>
-            </div>
-            <p></p>
-            <div>
-              <button onClick={excluirConf}>Excluir</button>
-            </div>
-            <p></p>
-
-            <button onClick={fecharModal}>Fechar</button>
-          </div >
-        </div>
-
-      )
-      }
     </>
 
   )
